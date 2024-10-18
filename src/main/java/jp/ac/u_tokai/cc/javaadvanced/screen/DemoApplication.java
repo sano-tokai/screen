bdf0610aca75b359ffafaa88499b9e193acdb2a1
sanot <sano@tokai-u.jp>
2024-10-18 16:23:30 +0900

    ドキュメンテーションコメントが漏れていたため追加

--- src/main/java/jp/ac/u_tokai/cc/javaadvanced/screen/DemoApplication.java
+++ src/main/java/jp/ac/u_tokai/cc/javaadvanced/screen/DemoApplication.java
@@ -3,9 +3,16 @@ package jp.ac.u_tokai.cc.javaadvanced.screen;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * クラスと画面の対応をとるサンプルプログラム
+ */
 @SpringBootApplication
 public class DemoApplication {
 
+	/**
+	 * Spring Bootを用いたWebアプリケーションの起動
+	 * @param args Spring Applicationの引数
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
